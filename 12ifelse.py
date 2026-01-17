53b79d9deada3cb9a2cd9e4a87cb2d1f778eeb00
pragnakalpdev85 <pragnakalp.dev85@gmail.com>
2026-01-17 16:45:35 +0530

    python basic tasks

--- 12ifelse.py
+++ 12ifelse.py
@@ -1,16 +1,16 @@
-# #input is greater or less than 10
-# num1 = int(input("enter a number: "))
-# if(num1 > 10):
-#     print("a is greater than 10")
-# else:
-#     print("a is less than 10")
+#input is greater or less than 10
+num1 = int(input("enter a number: "))
+if(num1 > 10):
+    print("a is greater than 10")
+else:
+    print("a is less than 10")
 
-# #taking input and checking the number is odd or even
-# num2 = int(input("enter a number: "))
-# if(num2%2 == 0):
-#     print("it is an even number")
-# else:
-#     print("it is an odd number")
+#taking input and checking the number is odd or even
+num2 = int(input("enter a number: "))
+if(num2%2 == 0):
+    print("it is an even number")
+else:
+    print("it is an odd number")
 
 #check the given number is prime or not
 num3 = int(input("enter a number: "))
